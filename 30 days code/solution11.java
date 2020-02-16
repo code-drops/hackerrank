279edd9bf4dee8ac2dac547f4616721a8236c5ce
chirag_kapoor <33838425+my-codebook@users.noreply.github.com>
2020-02-16 13:01:33 +0530

    Update solution11.java

--- 30 days code/solution11.java
+++ 30 days code/solution11.java
@@ -1,45 +1,37 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
-public class Solution {
-    private static final Scanner scanner = new Scanner(System.in);
+public class Day11 {
     public static void main(String[] args) {
-        int[][] arr = new int[6][6];
-        for (int i = 0; i < 6; i++) {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < 6; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
+        Scanner scanner = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = scanner.nextInt();
             }
         }
-        int max=0;
-        int s=0;
-        for(int r=0;r<=3;r++){
-            for(int c=0;c<=3;c++){
-                s=0;
+        
+        int sum = 0;
+        int max = -1000;
+        
+        for (int r=0; r<4; r++){
+            for (int c=0; c<4; c++){
+                sum=0;
                 for(int i=r;i<=r+2;i++){
                     for(int j=c;j<=c+2;j++){
                         if(((i==r+1)&&(j==c))||((i==r+1)&&(j==c+2))){
-                            s += 0 ;
+                            sum += 0 ;
                         }
                         else{
-                            s += arr[i][j];
+                            sum += arr[i][j];
                         }
                     }
                 }
-                if(max<s){
-                    max = s;
+                if(max<sum){
+                    max = sum;
                 }
             }
         }
-        System.out.println(max);
         scanner.close();
+        System.out.println(max);
     }
 }
