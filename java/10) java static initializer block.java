b5e03bf981a06ab24c8caf19faa51198717c45a0
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-04-08 22:18:58 +0530

    Update 10) java static initializer block.java

--- java/10) java static initializer block.java
+++ java/10) java static initializer block.java
@@ -1,4 +1,10 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
     static int B,H;
     static boolean flag;
     static Scanner scan = new Scanner(System.in);
@@ -12,4 +18,12 @@ static{
     else{
         System.out.println("java.lang.Exception: Breadth and height must be positive");
     }
+}
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}
 }
