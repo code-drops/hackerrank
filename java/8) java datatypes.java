50567d73040d4c32dbcdbf14f45b280e5fc339a7
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-04-08 22:15:47 +0530

    Update 8) java datatypes.java

--- java/8) java datatypes.java
+++ java/8) java datatypes.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-
 class Solution{
     public static void main(String []argh)
     {
@@ -8,12 +7,22 @@ class Solution{
         int t=sc.nextInt();
         for(int i=0;i<t;i++)
         {
-           try
+            try
             {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                //Complete the code
+                if(x>=(Byte.MIN_VALUE) && x<=Byte.MAX_VALUE){
+                    System.out.println("* byte");
+                }
+                if(x>=(Short.MIN_VALUE) && x<=Short.MAX_VALUE){
+                    System.out.println("* short");
+                }
+                if(x>=(Integer.MIN_VALUE) && x<=Integer.MAX_VALUE){
+                    System.out.println("* int");
+                }
+                if(x>=(Long.MIN_VALUE) && x<=Long.MAX_VALUE){
+                    System.out.println("* long");
+                }
             }
             catch(Exception e)
             {
