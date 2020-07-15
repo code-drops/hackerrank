36c060880ffdb77f9ae8b1b21d739659d35add29
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-15 14:27:26 +0530

    Create 01. Print the Elements of a Linked List.py

---
+++ Data structures/02. Linked Lists/01. Print the Elements of a Linked List.py
@@ -0,0 +1,55 @@
+'''
+If you're new to linked lists, this is a great exercise for learning about them. Given a pointer to the head node of a linked list, print its elements in order, one element per line. If the head pointer is null (indicating the list is empty), don’t print anything.
+'''#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+# Complete the printLinkedList function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def printLinkedList(head):
+    while head:
+        print(head.data)
+        head = head.next
+if __name__ == '__main__':
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    printLinkedList(llist.head)
