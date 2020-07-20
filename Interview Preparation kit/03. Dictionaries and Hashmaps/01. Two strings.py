bb1df3a0ca255d501106729ef7bbe52d82660829
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-20 17:33:25 +0530

    Create 01. Two strings.py

---
+++ Interview Preparation kit/03. Dictionaries and Hashmaps/01. Two strings.py
@@ -0,0 +1,35 @@
+'''
+Given two strings, determine if they share a common substring. A substring may be as small as one character.
+For example, the words "a", "and", "art" share the common substring a. The words "be" and "cat" do not share a substring.
+'''
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoStrings function below.
+def twoStrings(s1, s2):
+    l1 = [i for i in s1]
+    l2 = [i for i in s2]
+    if set(l1).intersection(set(l2)):
+        return 'YES'
+    else:
+        return 'NO'
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s1 = input()
+
+        s2 = input()
+
+        result = twoStrings(s1, s2)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
