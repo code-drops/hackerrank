4be748f6df22488fa6a7d59d327d7579f4cab48f
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-20 18:13:40 +0530

    Create 01. Say "Hello, World!" With C++.cpp

---
+++ C++/01. Introduction/01. Say "Hello, World!" With C++.cpp
@@ -0,0 +1,9 @@
+// We're starting out by printing the most famous computing phrase of all time! In the editor below, use either printf or cout to print the string "Hello, World!" to stdout.
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+int main() {
+    cout << "Hello, World!";
+    return 0;
+}
