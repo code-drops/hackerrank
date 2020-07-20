d61c59e295b9373d3d89dbf4c8bc2a0520867791
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-20 17:06:28 +0530

    Create 03. What's Your Name?.py

---
+++ python/03. Strings/03. What's Your Name?.py
@@ -0,0 +1,9 @@
+'''
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+Hello firstname lastname! You just delved into python.
+'''
+
+def print_full_name(a, b):
+    print("Hello "+a+" "+b+"! You just delved into python.")
+
+if __name__ == '__main__':
