16265c71ad7f562c26fb846aac332f1377a7cc8d
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-20 17:20:22 +0530

    Create 03. Insert a node at the head of a linked list.py

---
+++ Data structures/02. Linked Lists/03. Insert a node at the head of a linked list.py
@@ -0,0 +1,51 @@
+'''
+You’re given the pointer to the head node of a linked list and an integer to add to the list. Create a new node with the given integer, insert this node at the head of the linked list and return the new head node. The head pointer given may be null meaning that the initial list is empty.
+'''
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+def insertNodeAtHead(head, data):
+    # Write your code here
+    temp = SinglyLinkedListNode(data)
+    temp.next = head
+    head = temp
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtHead(llist.head, llist_item)
+        llist.head = llist_head
+    
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+    
+    fptr.close()
