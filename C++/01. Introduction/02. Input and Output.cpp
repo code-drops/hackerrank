0516c9362ae1f7c929613f222dc17cae34f9e03b
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-21 17:17:14 +0530

    Create 02. Input and Output.cpp

---
+++ C++/01. Introduction/02. Input and Output.cpp
@@ -0,0 +1,16 @@
+// Read 3 numbers from stdin and print their sum to stdout.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << (a+b+c); 
+    return 0;
+}
