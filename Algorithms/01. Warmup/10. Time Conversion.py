77694a8a484fed6bdd3edbd70e62bbe409efc898
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-21 14:04:21 +0530

    Create 10. Time Conversion.py

---
+++ Algorithms/01. Warmup/10. Time Conversion.py
@@ -0,0 +1,27 @@
+'''
+Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+
+Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock. Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+'''
+import os
+import sys
+
+def timeConversion(s):
+    time = s.split(":")
+    if s[-2:] == "PM":
+        if time[0] != "12":
+            time[0] = str(int(time[0])+12)
+    else:
+        if time[0] == "12":
+            time[0] = "00"
+    ntime = ':'.join(time)
+    return str(ntime[:-2])
+    
+if __name__ == '__main__':
+    f = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = timeConversion(s)
+
+    f.write(result + '\n')
