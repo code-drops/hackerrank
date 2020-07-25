5a6dda49b26ee004ebf657491742555150eca94d
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-25 19:46:16 +0530

    Create 1. Positive Lookahead.py

---
+++ regex/06. Assertions/1. Positive Lookahead.py
@@ -0,0 +1,10 @@
+'''
+You have a test string S.
+Write a regex that can match all occurrences of o followed immediately by oo in S.
+'''
+
+Regex_Pattern = r'o(?=oo)'
+import re
+Test_String = input()
+match = re.findall(Regex_Pattern, Test_String)
+print("Number of matches :", len(match))
