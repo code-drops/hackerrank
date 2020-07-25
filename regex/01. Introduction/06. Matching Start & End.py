9fd226048a27ba091c93ed222508a395da6d06ec
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-25 18:42:15 +0530

    Create 06. Matching Start & End.py

---
+++ regex/01. Introduction/06. Matching Start & End.py
@@ -0,0 +1,10 @@
+'''
+You have a test string S. Your task is to match the pattern Xxxxx 
+Here, x denotes a word character, and X denotes a digit.
+S must start with a digit X and end with . symbol.
+S should be 6 characters long only.
+'''
+
+Regex_Pattern = r"^\d\w{0,4}\.$"	# Do not delete 'r'.
+import re
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
