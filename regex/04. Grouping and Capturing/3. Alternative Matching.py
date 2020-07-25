305cb3e3cf6ec756b3ec7d63949111d011e522cf
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-25 19:32:07 +0530

    Create 3. Alternative Matching.py

---
+++ regex/04. Grouping and Capturing/3. Alternative Matching.py
@@ -0,0 +1,12 @@
+'''
+Given a test string,s , write a RegEx that matches s under the following conditions:
+
+S must start with Mr., Mrs., Ms., Dr. or Er..
+The rest of the string must contain only one or more English alphabetic letters (upper and lowercase).
+'''
+
+Regex_Pattern = r'^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]{1,}$'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
