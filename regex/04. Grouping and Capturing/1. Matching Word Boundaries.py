a07baa72054946e8743ea5c12e6cab1b7310cdfe
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-25 19:30:09 +0530

    Create 1. Matching Word Boundaries.py

---
+++ regex/04. Grouping and Capturing/1. Matching Word Boundaries.py
@@ -0,0 +1,12 @@
+'''
+You have a test String S.
+Your task is to write a regex which will match word starting with vowel (a,e,i,o, u, A, E, I , O or U).
+The matched word can be of any length. The matched word should consist of letters (lowercase and uppercase both) only.
+The matched word must start and end with a word boundary.
+'''
+
+Regex_Pattern = r'\b[AEIOUaeiuo][a-zA-Z]*\b'	# Do not delete 'r'.
+
+import re
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
