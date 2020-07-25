0b5687bef3c276196d0d3f0cc1af92b8b69a7636
chirag_kapoor <33838425+code-drops@users.noreply.github.com>
2020-07-25 18:46:53 +0530

    Create 02. Excluding Specific Characters.py

---
+++ regex/02. Character Class/02. Excluding Specific Characters.py
@@ -0,0 +1,16 @@
+'''
+You have a test string S.
+Your task is to write a regex that will match S with the following conditions:
+
+S must be of length 6.
+First character should not be a digit (1,2,3,4,5,6,7,8,9 or 0 ).
+Second character should not be a lowercase vowel ( a,e,i,o or u ).
+Third character should not be b, c, D or F.
+Fourth character should not be a whitespace character ( \r, \n, \t, \f or <space> ).
+Fifth character should not be a uppercase vowel ( A,E,I,O or U ).
+Sixth character should not be a . or , symbol.
+'''
+
+Regex_Pattern = r'^[^1234567890][^aeiou][^bcDF][^\s][^AEIOU][^.,]$'
+import re
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
